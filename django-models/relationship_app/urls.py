ce2c6c0af5418f4a89e81968ab980ab9c2c42c4d
Samuel Victor <35655887+Derilee@users.noreply.github.com>
2024-08-25 22:11:29 +0100

    new changes

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     path('register/', views.register, name='register'),
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='logout.html', next_page='login'), name='logout'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html', next_page='login'), name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,10 +4,20 @@ from .models import Book
 # Create your views here.
 
 
-def book_list(request):
+def list_books(request):
     books = Book.objects.all()
     context = {'book_list': books}
-    return render(request, 'books/list_books.html', context)
+    return render(request, 'relationship_app/list_books.html', context)
+
+
+from django.views.generic import DetailView
+from .models import Library
+
+class LibraryDetailView(DetailView):
+    model = Library 
+    template_name = 'relationship_app/library_detail.html' 
+    context_object_name = 'library'
+
 
 
 from django.shortcuts import render, redirect
@@ -16,7 +26,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'relationship_app/home.html')
 
 def register(request):
     if request.method == 'POST':
@@ -27,7 +37,7 @@ def register(request):
             return redirect('home')
     else:
         form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
+    return render(request, 'relationship_app/register.html', {'form': form})
 
 def user_login(request):
     if request.method == 'POST':
@@ -38,12 +48,12 @@ def user_login(request):
             return redirect('home')
     else:
         form = AuthenticationForm()
-    return render(request, 'login.html', {'form': form})
+    return render(request, 'relationship_app/login.html', {'form': form})
 
-def user_logout(request):
-    logout(request)
-    return redirect('login')
+# def user_logout(request):
+#     logout(request)
+#     return redirect('relationship_app/login')
 
 @login_required
 def profile(request):
-    return render(request, 'profile.html')
+    return render(request, 'relationship_app/profile.html')
