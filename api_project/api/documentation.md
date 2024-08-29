d8b8442a651a5fe14078cd0aa018a0a87e42ad7c
Samuel Victor <35655887+Derilee@users.noreply.github.com>
2024-08-29 14:43:20 +0100

    Introduction to Building APIs with Django REST Framework

---
+++ api_project/api/documentation.md
@@ -0,0 +1,9 @@
+How to test the API using a REST client or curl commands
+
+Rest client
+- Run the server
+- ![Sever Running](Screenshot1.png)
+- Now click on this link, http://127.0.0.1:8000/api/BookList/?format=api
+- Now enter a Book title and Author and click on Post
+- ![Post a Book](Screenshot2.png)
+
--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from .views import BookList, BookViewSet
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'BookList', BookViewSet)
+
 
 urlpatterns = [
     path('api/books/', BookList.as_view(), name='book-list'),
+    path('api/', include(router.urls))
 ]
