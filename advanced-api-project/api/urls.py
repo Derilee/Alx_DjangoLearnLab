31d5409520929042105d4e0afbd5a686be55f848
Samuel Victor <35655887+Derilee@users.noreply.github.com>
2024-09-06 23:37:30 +0100

    Advanced API Development with Django REST Framework

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,12 +4,12 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
-    path('authors/', AuthorListView.as_view(), name='author-list'),
-    path('authors/<int:pk>/', AuthorDetailView.as_view(),name='author-detail'),
-    path('books/', BookListView.as_view(), name='book-list'),
-    path('books-create/', BookCreateView.as_view(), name='create-books'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='books-details'),
-    path('books-delete/<int:pk>/', BookDeleteView.as_view(), name='delete-books'),
-    path('books-update/<int:pk>/', BookUpdateView.as_view(), name='update-books'),
+    path('authors', AuthorListView.as_view(), name='author-list'),
+    path('authors/<int:pk>', AuthorDetailView.as_view(),name='author-detail'),
+    path('books', BookListView.as_view(), name='book-list'),
+    path('books/create', BookCreateView.as_view(), name='create-books'),
+    path('books/<int:pk>', BookDetailView.as_view(), name='books-details'),
+    path('books/delete/<int:pk>', BookDeleteView.as_view(), name='delete-books'),
+    path('books/update/<int:pk>', BookUpdateView.as_view(), name='update-books'),
     path('api-token-auth/', obtain_auth_token, name='auth-token-auth'),
 ]
