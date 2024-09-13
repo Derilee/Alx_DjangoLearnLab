6d4cf108c522e40fbfbcd89b4b3e0a87633a7222
Samuel Victor <35655887+Derilee@users.noreply.github.com>
2024-09-13 18:51:02 +0100

    Building a Complete Django Application

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('post/', views.BlogListView.as_view(), name='posts_list'),
     path('post/new/', views.BlogCreateView.as_view(), name='post_create'),
     path('post/<int:pk>/', views.BlogDetailView.as_view(), name='post_detail'),
-    path('post/<int:pk>/edit/', views.BlogUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/update/', views.BlogUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/delete/', views.BlogDeleteView.as_view(), name='post_delete'),
 ]
