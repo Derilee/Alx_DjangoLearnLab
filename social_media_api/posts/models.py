1b3c4d489b45f8bf7c109aa77d7ddeda1b2238eb
Samuel Victor <35655887+Derilee@users.noreply.github.com>
2024-09-19 03:38:02 +0100

    Building and deploying a Django API

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -5,14 +5,14 @@ from django.conf import settings
 class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='posts', on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
-    content = models.TextField(blank=True)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='comments', on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    content = models.TextField(blank=True)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
 
